260a3dde1b3b092bdb8a2003f1bbc75fcdb4142a
decanoe <deca.noe49@gmail.com>
2024-05-01 14:34:11 +0200

    removing the build folder from the commits

--- build/CMakeFiles/VoxelEngine.dir/class/world/materials.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/VoxelEngine.dir/class/world/materials.cpp.obj: \
- C:\Users\decan\Documents\ExtraScolaire\c++\VoxelEngine\class\world\materials.cpp \
- C:/Users/decan/Documents/ExtraScolaire/c++/VoxelEngine/class/world/materials.h
